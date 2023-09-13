c57699059850fee7eeff626a9175e17dfcac1f44
Keshava9154 <109907993+Keshava9154@users.noreply.github.com>
2023-09-13 23:39:42 +0530

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,27 @@
+/**
+ * index.mjs 
+ **/
+ /**const SecretsManager = require('./SecretsManager.js');**/
+ 
+ import secretsManager from './secretsManager.js';
+
+ exports.handler = async (event) => {
+     // TODO implement
+     var secretName = 'Development';
+     var region = 'us-east-2';
+     var apiValue = await secretsManager.getSecret(secretName, region);
+     console.log(apiValue); 
+     const response = {
+         statusCode: 200,
+         body: JSON.stringify('Hello from Lambda!'),
+     };
+     return response;
+ };
+
+export const handler = async function(event, context) {
+  event.Records.forEach(record => {
+    const { body } = record;
+    console.log(body);
+  });
+  return {};
+};
